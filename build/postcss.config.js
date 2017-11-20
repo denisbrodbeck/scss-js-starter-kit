739dcd850329c2eb72a12073c611c6c51f2af88a
Denis Brodbeck <denis@shahaya.com>
2017-11-20 13:17:44 +0100

    Add working scss part
    JS still missing.

---
+++ build/postcss.config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = (ctx) => ({
+  map: {
+    inline: false,
+    annotation: true,
+    sourcesContent: true
+  },
+  plugins: {
+    autoprefixer: { cascade: false }
+  }
+})
